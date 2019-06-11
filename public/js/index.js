e44f7029b6261904c97250fd89cf1afcdde43d5d
IsaacYocum <isyocum@gmail.com>
2019-06-10 20:03:43 -0600

    Added database using nedb

--- public/js/index.js
+++ public/js/index.js
@@ -2,22 +2,52 @@ $(() => {
     console.log('HI')
 
     $("body").keydown((e) => {
-        if (e.which == 38) {
-            console.log("keyleft")
-            getRandomWord();
-        }
+        if (e.which == 38) {getAllWords();}
+        if (e.which == 39) {getRandomWord();}
     })
 
+    $('#submitNewWordBtn').click(() => {
+        let newTrans = {
+            viet: $('#vWord').val().toLowerCase(),
+            eng: $('#eWord').val().toLowerCase()
+        }
+
+        console.log(newTrans)
+
+        $.ajax({
+            url: '/newWord',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(newTrans),
+            success: (data) => {
+                console.log(data);
+            }
+        });
+    })
+
+    function getAllWords() {
+        $.getJSON('/getAllWords').then((resp) => populateTable(resp, resp.length));
+    }
+
     function getRandomWord() {
-        $.getJSON('/getWords').then((resp) => {
-            console.log(resp.words[1].eng);
-            let random = Math.floor(Math.random() * (resp.words.length - 0));
+        $.getJSON('/getRandomWord').then((resp) => populateTable(resp, resp.length));
+    }
+
+    function populateTable(data, num) {
+        $('tbody').empty()
+
+        for (let i = 0; i < num; i++) {
             $('tbody').append(`
                 <tr>
-                    <td>${resp.words[random].viet}</td>
-                    <td>${resp.words[random].eng}</td>
+                    <td class="col-lg-5">${data[i].viet}</td>
+                    <td class="col-lg-5">${data[i].eng}</td>
+                    <td class="col-lg-2" id="${data[i]._id}">
+                        <button class="btn btn-warning">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
                 </tr>
             `);
-        })
+        }
     }
 })
--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
-const fs = require('fs');
-const server = app.listen(3000, () => {
-    console.log("Server listening on port: " + server.address().port)
-});
+const db = new Datastore('words.db');
 
+app.listen(3000, () => console.log("Server listening on port: "));
 app.use(express.static('public'));
 app.use(express.json())
 
+db.loadDatabase();
+
 app.post('/newWord', (req, res) => {
-    console.log(req.body)
+    db.insert(req.body, (err) => {
+        if (err) {
+            console.log(err);
+            res.end();
+            return
+        }
+    })
+    res.send({addWord: 'Successful'})
 })
+
+app.get('/getAllWords', (req, res) => {
+    db.find({}, (err, docs) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.send(docs);
+    })
+})
+
+app.get('/getRandomWord', (req, res) => {
+    db.find({}, (err, docs) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        let rand = Math.floor(Math.random() * Math.floor(docs.length));
+        res.send([docs[rand]]);
+    })
+})
+
+
 
 
 
